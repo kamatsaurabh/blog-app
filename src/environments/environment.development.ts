4f1df4ec707cc271b113ea7125b92c1ca3cdee60
kamatsaurabh <saurabh.kamat@intelegencia.com>
2025-02-24 21:33:33 +0530

    Fixed keys and readme

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   Url:'http://localhost:3000',
-  GoogleAuthKey:'1052149523397-o5o0nn1gj9m9le52189q68geqo73veoi.apps.googleusercontent.com',
-  FacebookAuthKey:'1296177931495483'
+  GoogleAuthKey:'',
+  FacebookAuthKey:''
 };
