b38366e776375283e4aedc593b4cd8d1f1c102b1
kamatsaurabh <saurabh.kamat@intelegencia.com>
2025-02-24 15:58:09 +0530

    app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./pages/login/login.component";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { CreatePostComponent } from "./pages/create-post/create-post.component";
+import { PostDetailComponent } from "./pages/post-detail/post-detail.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'create-post', component: CreatePostComponent },
+  { path: 'post/:id', component: PostDetailComponent }
+];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,40 @@ import { NgModule, provideZoneChangeDetection, inject } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SocialLoginModule, SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
+import { GoogleLoginProvider, FacebookLoginProvider } from '@abacritt/angularx-social-login';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false, // Set to true if you want auto-login
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('1052149523397-o5o0nn1gj9m9le52189q68geqo73veoi.apps.googleusercontent.com'),
+          },
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider('1296177931495483'),
+          }
+        ]
+      } as SocialAuthServiceConfig
+    }
   ],
   declarations: [
     AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    SocialLoginModule,
+    CommonModule 
   ],
   bootstrap: [AppComponent]
 })
